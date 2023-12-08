66fff919c653c56ec5e3c6897bf8c7b120606cba
Alpy81 <alperen.ertas@gmx.de>
2023-12-08 18:15:28 +0100

    update commit

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<?php 
-   session_start();
+<?php
+    session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,37 +11,36 @@
     <link rel="stylesheet" href="style/style.css">
     <title>Login</title>
 </head>
-<body>
-      <div class="container">
-        <div class="box form-box">
-            <?php 
-             
-              include("php/config.php");
-              if(isset($_POST['submit'])){
-                $email = mysqli_real_escape_string($con,$_POST['email']);
-                $password = mysqli_real_escape_string($con,$_POST['password']);
 
-                $result = mysqli_query($con,"SELECT * FROM users WHERE Email='$email' AND Password='$password' ") or die("Select Error");
+<body>
+    <div class="container">
+        <div class="box form-box">
+            <?php
+              ("php/config.php");
+              if(isset($_POST['submit'])){
+                $email = mysqli_real_escape_string($con, $_POST['email']);
+                $password = mysqli_real_escape_string($con, $_POST['password']);
+
+                $result = mysqli_query($con, "SELECT * FROM users WHERE Email='$email'") or die("Select Error");
                 $row = mysqli_fetch_assoc($result);
 
-                if(is_array($row) && !empty($row)){
+                if($row && password_verify($password, $row['Password'])){
+                    session_regenerate_id(true);
+
                     $_SESSION['valid'] = $row['Email'];
                     $_SESSION['username'] = $row['Username'];
                     $_SESSION['age'] = $row['Age'];
                     $_SESSION['id'] = $row['Id'];
-                }else{
-                    echo "<div class='message'>
-                      <p>Wrong Username or Password</p>
-                       </div> <br>";
-                   echo "<a href='index.php'><button class='btn'>Go Back</button>";
-         
-                }
-                if(isset($_SESSION['valid'])){
-                    header("Location: home.php");
-                }
-              }else{
 
-            
+                    header("Location: home.php");
+                    exit();
+                } else {
+                    echo "<div class='message'>
+                          <p>Wrong Username or Password</p>
+                          </div> <br>";
+                   echo "<a href='index.php'><button class='btn'>Go Back</button>";
+                }
+              }
             ?>
             <header>Login</header>
             <form action="" method="post">
@@ -55,15 +55,14 @@
                 </div>
 
                 <div class="field">
-                    
-                    <input type="submit" class="btn" name="submit" value="Login" required>
+                    <input type="submit" class="btn" name="submit" value="Login">
                 </div>
                 <div class="links">
-                    Don't have account? <a href="register.php">Sign Up Now</a>
+                    Don't have an account? <a href="register.php">Sign Up Now</a>
                 </div>
             </form>
         </div>
-        <?php } ?>
-      </div>
+    </div>
 </body>
+
 </html>
